8937e323fb624ab6caa5167d9be3bc4bc8d7d04e
Jordan S Read <jread@usgs.gov>
2015-01-02 14:01:47 -0600

    generic handler. returns both types def empty

--- R/get_surface_temps.R
+++ R/get_surface_temps.R
@@ -1,6 +1,6 @@
 #'@title get summer surface lake temperature data from GLTC dataset
 #'@param lake_name a valid name of a lake in the GLTC database
-#'@param type source for the data. Either "in_situ" or "satellite".
+#'@param type source for the data. Either "In situ" or "Satellite".
 #'@return a lake data.frame, or NULL if no data exist
 #'@examples
 #'get_surface_temps('Victoria','Satellite')
@@ -16,27 +16,8 @@ get_surface_temps <- function(lake_name, type){
   
   if (!all(type %in% both)){stop(paste0('type=', type, ' not recognized'))}
   
-  data(gltc_values)
-  check_lake(lake_name)
-  
-  df <- filter(gltc_values, variable %in% type, siteID == get_site_ID(lake_name)) %>%
-    select(variable, year, value) %>% 
-    acast(year ~ variable)
-  df <- cbind(data.frame(year = as.numeric(row.names(df))), df)
-  
-  rownames(df) <- NULL
-  
-  
-  surf_temps <- df
-  
-  if (is.null(surf_temps)){
-    return(NULL)
-  } else {
-    return(as.data.frame(surf_temps))
-  }
+  surf_temps <- subset_lake_data(lake_name, types = type)
+  return(surf_temps)
   
 }
 
-temp_types = function(){
-  return(c('Satellite','In situ'))
-}
