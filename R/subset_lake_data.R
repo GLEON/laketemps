e3552541b8d4799070683f42aa6933b7ea7eb8e6
Jordan S Read <jread@usgs.gov>
2015-01-05 09:28:52 -0600

    using sysdata

--- R/subset_lake_data.R
+++ R/subset_lake_data.R
@@ -1,9 +1,6 @@
 subset_lake_data = function(lake_name, types){
   
-  data(gltc_values)
-  check_lake(lake_name)
-  
-  
+  check_lake(lake_name)  
   
   df = tryCatch({
     df <- filter(gltc_values, variable %in% types, siteID == get_site_ID(lake_name)) %>%
